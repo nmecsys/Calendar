1bf26b9543b64fc166ce80e6c968be19b85b3e26
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-10-08 17:04:09 -0300

    Update calendar.py

--- src/calendar.py
+++ src/calendar.py
@@ -1,1 +1,19 @@
 # python calendar version
+import pandas as pd
+import datetime
+
+
+def tranding_days(start = "2001-01-01", finish ='2050-12-31',feriados = None ,fds = ['saturday','sunday']):
+    if():
+      return("Dias uteis abaixo de janeiro de 2001 indisponiveis!")
+    
+    if(feriados != None ):
+      cal  =
+    else:
+      cal  =
+   
+  .
+  .
+  .
+  .
+  
