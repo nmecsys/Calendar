07954274e951ddff31a4d8b6c16882a5ea8a71f8
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-10-09 11:20:57 -0300

    Update calendar.py

--- src/calendar.py
+++ src/calendar.py
@@ -12,8 +12,30 @@ def tranding_days(start = "2001-01-01", finish ='2050-12-31',feriados = None ,fd
     else:
       cal  =
    
-  .
-  .
-  .
-  .
-  
+     dias  = bizseq(from, to, cal)
+     datas = seq.Date(as.Date(from),as.Date(to),by = 'month')
+      
+      dias_d = pd.DataFrame(columns = ['dias','x'])
+      final  = pd.DataFrame(columns = ['data','count'])
+    
+      dias_d.dias = dias  
+      dias_d.x = Nan
+      final.data = datas
+      final.count = Nan
+        
+
+      for i in range(len(final)):
+        #count = dias_d %>% filter(format(dias,"%Y-%m") == format(final$data[i],"%Y-%m")) %>% select(dias) %>% count()
+        #message(paste(yellow(i),"-",yellow(final$data[i]),"-","dias uteis:",green(count),sep=" "))
+        #final$count[i] = count
+     
+
+
+      if(ts):
+      #  ano = format(from,"%Y") %>% as.numeric()
+      #  mes  = format(from,"%m") %>% as.numeric()
+      #  serie_dias_uteis = ts(final$count,start = c(ano,mes),frequency = 12)  
+      #  return(serie_dias_uteis)
+      
+      return(final)
+
